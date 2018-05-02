d9f6ede8e6345b9029204403e6bd9e99aef43670
Isaac Duarte <iduarte@cs.stonybrook.edu>
2018-05-02 08:56:09 -0700

    Finished part 1. May need to handle more error cases

--- main.c
+++ main.c
@@ -5,41 +5,88 @@
 #define MEM  "fifo_mem"
 
 typedef struct pt2 {
-	uint * address;
-	int valid;
+	uint address[1024];
+	int valid[1024];
+	int full;
 } pt2;
 
 typedef struct pt1 {
-	pt2 * table2;
-	int valid;
+	pt2 * table2[1024];
 } pt1;
 
 typedef struct process {
 	pthread_t tid;
 	pt1 table1;
+	uint virtuals[1024];
+	int size;
 } process;
 
 process threads[4];
 int size = 0;
 
-uint cse320_malloc(){
-	return 0;
+uint cse320_malloc(process * p, uint addr){
+	uint i1, i2;
+
+	for (i1 = 0; i1 < 1024; i1++){
+		if (p->table1.table2[i1] != NULL && p->table1.table2[i1]->full == 0	){
+			break;
+		}
+		else if ((p->table1.table2[i1] == NULL)){
+			p->table1.table2[i1] = malloc(sizeof(pt2));
+			break;
+		}
+	}
+	for (i2 = 0; i2 < 1024; i2++){
+		if (p->table1.table2[i1]->valid[i2] == 0){
+			p->table1.table2[i1]->address[i2] = addr;
+			p->table1.table2[i1]->valid[i2] = 1;
+			break;
+		}
+	}
+	if (i2 == 1024)
+		p->table1.table2[i1]->full = 1;
+
+	uint virt = (i1 << 22) | (i2 << 12) | 0;
+	return virt;
 }
 
 uint cse320_virt_to_phys(uint virt, process p){
-
-	return 0;
+	uint t1Index = virt >> 22;
+	uint t2Index = (virt >> 12) & 1023;
+	pt2 table2 = *(p.table1.table2[t1Index]);
+	uint phys = table2.address[t2Index];
+	return phys;
 }
 
 void * handler(void * arg){
 	
 }
 
-process findProcess(pthread_t tid){
+process * findProcess(pthread_t tid, int * index){
 	int i;
 	for (i = 0; i < 4; i++){
-		if (threads[i].tid == tid)
-			return threads[i];
+		if (threads[i].tid == tid){
+			if (index != NULL)
+				*index = i;
+			return &threads[i];
+		}
+	}
+	return NULL;
+}
+
+void sendMessage(char * message){
+	FILE * mem = fopen(MEM, "w");
+	fputs(message, mem);
+	fclose(mem);
+}
+
+void cleanThreads(){
+	int i, j;
+	for (i = 0; i < 4; i++){
+		if (threads[i].tid == 0) continue;
+		for (j = 0; threads[i].table1.table2[j] != NULL; j++)
+			free(threads[i].table1.table2[j]);
+		pthread_join(threads[i].tid, NULL);
 	}
 }
 
@@ -52,22 +99,19 @@ int main(int argc, char** argv){
 			exit(0);
 		}
 	}
-	FILE * main = fopen(MAIN, "r");
-	FILE * mem = fopen(MEM, "w");
-	char buf[250];
-	while(1){
-		printf("prompt> ");
-		scanf(" %s", buf);
-		fputs(buf, mem);
-	}
+	FILE * main;
 	
 	uint phys;
 	char phy[11];
-	char input[256];
+	char input[256], buff[256];
 	char* args[4];
-	int i, j, result, free = 0;
+	int i, j, result, f = 0;
+	process * p;
 
-	for (i = 0; i < 4; i++) threads[i].tid = 0;
+	for (i = 0; i < 4; i++) {
+		threads[i].tid = 0;
+		threads[i].size = 0;
+	}
 
 	while(1){
 		printf("prompt> ");
@@ -86,50 +130,89 @@ int main(int argc, char** argv){
 		args[0] = &input[0];
 
 		if (strcmp(args[0], "create") == 0){
-			if (size >= 4) {
+			if (size >= 4){
 				printf("No more processes can be created\n");
-				break;
+				continue;
 			}
 			for (i = 0; i < 4; i++) 
 				if (threads[i].tid == 0) {
-					free = i;
+					f = i;
 					break;
 				}
-			pthread_create(&threads[free].tid, NULL, &handler, NULL);
+			pthread_create(&(threads[f].tid), NULL, &handler, NULL);
 			size++;
-		}
+		}	
 		else if (strcmp(args[0], "kill") == 0){
 			pthread_t thread = strtoul(args[1], NULL, 10);
-			printf("%lu\n", thread);
-			pthread_join(thread, NULL);
-			for (i = 0; i < 4; i++) 
-				if (threads[i].tid == thread){
-					threads[i].tid = 0;
-					break;
+			p = findProcess(thread, &i);
+			if (p != NULL){
+				pthread_join(thread, NULL);
+				sprintf(buff, "kill 0 %d", i);
+				sendMessage(buff);
+				main = fopen(MAIN, "r");
+				fclose(main);
+				threads[i].tid = 0;
+				for (i = 0; p->table1.table2[i] != NULL; i++){
+					free(p->table1.table2[i]);
 				}
-			size--;
+				size--;
+			} else
+				printf("That process does not exist\n");
 		}
 		else if (strcmp(args[0], "list") == 0){
 			for (i = 0; i < 4; i++) if (threads[i].tid != 0) printf("Process %lu\n", threads[i].tid);
 		}
 		else if (strcmp(args[0], "mem") == 0){
-			;
+			p = findProcess(strtoul(args[1], NULL, 10), NULL);
+			if (p == NULL) {
+				printf("Not a valid process\n");
+				continue;
+			}
+			int vsize = p->size;
+			for (i = 0; i < vsize; i++)
+				printf("Virtual Address: 0x%X\n", p->virtuals[i]);
 		}
 		else if (strcmp(args[0], "allocate") == 0){
-			cse320_malloc();
+			p = findProcess(strtoul(args[1], NULL, 10), &i);
+			if (p == NULL) {
+				printf("Not a valid process\n");
+				continue;
+			}
+			sprintf(buff, "request 4 %d", i);
+			sendMessage(buff);
+			main = fopen(MAIN, "r");
+			fscanf(main, "%u %s", &phys, buff);
+			if (input[0] == 'E') 
+				printf("%s for process\n", buff);
+			else {
+				uint virt = cse320_malloc(p, phys);
+				printf("Virtual address 0x%X or %d created for process %s\n", virt, virt, args[1]);
+				p->virtuals[p->size] = virt;
+				p->size++;
+			}
+			fclose(main);
 		}
 		else if (strcmp(args[0], "read") == 0){
-			phys = cse320_virt_to_phys(atoi(args[1]), args[2]);
-			sprintf(phy, "%d", phys);
-			fputs(phy, mem);
-			fscanf(main, "%d", &result);
-			printf("%d", result);
-		}
-		else if (strcmp(args[0], "write") == 0)
-			;
-		else if (strcmp(args[0], "exit") == 0){
+			phys = cse320_virt_to_phys(atoi(args[2]), *findProcess(strtoul(args[1], NULL, 10), NULL));
+			sprintf(input, "%s %u", args[0], phys);
+			sendMessage(input);
+			main = fopen(MAIN, "r");
+			while(fgets(input, 256, main) > 0);
+			printf("%s\n", input);
 			fclose(main);
-			fclose(mem);
+		}
+		else if (strcmp(args[0], "write") == 0){
+			phys = cse320_virt_to_phys(atoi(args[2]), *findProcess(strtoul(args[1], NULL, 10), NULL));
+			sprintf(input, "%s %u %s", args[0], phys, args[3]);
+			sendMessage(input);
+			main = fopen(MAIN, "r");
+			fclose(main);
+		}
+		else if (strcmp(args[0], "exit") == 0){
+			cleanThreads();
+			sendMessage("exit");
+			main = fopen(MAIN, "r");
+			fclose(main);
 			exit(0);
 		}
 	}
--- mem.c
+++ mem.c
@@ -3,17 +3,53 @@
 #define MEM  "fifo_mem"
 
 int main(){
-	sleep(5);
 	void * memory = calloc(1024, 1);
+	FILE * main, * mem;
+	unsigned int address;
+	int write, i, bound;
+	
+	int points[4] = {0, 256, 512, 768};
 
-	FILE * main = fopen(MAIN, "w");
-	FILE * mem;
 	char buf[250];
 	while(1){
 		mem = fopen(MEM, "r");
-		fgets(buf, 80, mem);
+		main = fopen(MAIN, "w");
+		
+		fscanf(mem, "%s %u %d", buf, &address, &write);
+		if (strcmp(buf, "exit") == 0) {
+			free(memory);
+			fclose(mem);
+			fclose(main);
+			return 0;
+		}
+		sleep(5);
+		printf("%s %u %d\n", buf, address, write);
+		if (address % 4 != 0){
+			printf("error,address is not aligned\n");
+		} else if (address >= 1024){
+			printf("error,address out of range\n");
+		}
+		else if (strcmp(buf, "read") == 0) {
+			sprintf(buf, "%d", (*(int *)(memory + address)));
+			fputs(buf, main);
+		} else if (strcmp(buf, "write") == 0){
+			(*(int*)(memory + address)) = write;
+		} else if (strcmp(buf, "request") == 0){
+			i = points[write];
+			if (i < 256 * (write + 1)){
+				sprintf(buf, "%d", i);
+				fputs(buf, main);
+				points[write] = i + 4;
+			}
+			else{
+				fputs("Error No Memory", main);
+			}
+		} else if (strcmp(buf, "kill") == 0){
+			points[write] = write * 256;
+		}
 		printf("received %s\n", buf);
 		fclose(mem);
+		fclose(main);
 	}
 	return 0;
 }
